c5201f8834cb04f62710b7f04a66cd5b1267f80a
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2024-01-10 06:27:34 -0300

    Refactor stub and update dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
-interface Index {
-    value: boolean;
+class Index {
+    private readonly value: boolean;
+
+    public constructor() {
+        this.value = true;
+    }
 }
 
-const index: Index = {
-    value: true,
-};
+const index = new Index();
 
 export default index;
